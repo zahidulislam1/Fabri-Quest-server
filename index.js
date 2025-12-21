d2ce24f4f53cd3552aaea0f8b2992a7d709f8f50
zahiduislam1 <zahidulislam211072@gmail.com>
2025-12-22 04:14:32 +0600

    product post ar user post done

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jmftqsk.mongodb.net/?appName=Cluster0S`;
 
+// jwt middlewares
+// const verifyJWT = async (req, res, next) => {
+//   const token = req?.headers?.authorization?.split(' ')[1]
+//   console.log(token)
+//   if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+//   try {
+//     const decoded = await admin.auth().verifyIdToken(token)
+//     req.tokenEmail = decoded.email
+//     console.log(decoded)
+//     next()
+//   } catch (err) {
+//     console.log(err)
+//     return res.status(401).send({ message: 'Unauthorized Access!', err })
+//   }}
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,7 +43,47 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const 
+    const db = client.db("fabri-quest-db");
+    const productsCollection = db.collection("products");
+    const usersCollection = db.collection("users");
+
+    //save a product data in db
+    app.post("/products", async (req, res) => {
+      const productData = req.body;
+      const result = await productsCollection.insertOne(productData);
+      res.send(result);
+    });
+    //save a user data in db
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      userData.created_at = new Date().toISOString();
+      userData.last_loggedIn = new Date().toISOString();
+      userData.role = "buyer";
+
+      const query = {
+        email: userData.email,
+      };
+
+      const alreadyExists = await usersCollection.findOne(query);
+
+      if (alreadyExists) {
+        const result = await usersCollection.updateOne(query, {
+          $set: {
+            last_loggedIn: new Date().toISOString(),
+          },
+        });
+        return res.send(result);
+      }
+
+      const result = await usersCollection.insertOne(userData);
+      res.send(result);
+    });
+
+    //get all products form db
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
